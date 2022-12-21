534b28da5ea10ee9aca09d665e194037ea1946cc
Yandall <js.agurod@gmail.com>
2022-12-21 15:51:59 -0500

    Docs added to source code

--- htest.nimble
+++ htest.nimble
@@ -14,3 +14,10 @@ requires "nim >= 1.6.8", "nimquery"
 task test, "Run tests":
   exec "nim c -r --verbosity:0 tests/tests"
   rmFile "tests/tests"
+
+task docs, "Write the package docs":
+  exec "nim doc --verbosity:0 --project --index:on " &
+    "--git.url:https://github.com/Yandall/HTest " &
+    "--git.commit:main " &
+    "-o:htmldocs " &
+    "src/htest.nim"
--- src/htest.nim
+++ src/htest.nim
@@ -9,52 +9,67 @@ type
     useNot: bool
 
 proc check*(html: string): HTest =
+  ## Use when you want to test for a `html` string
   let xml = html.parseHtml()
   result = HTest(html: xml, useNot: false)
 
 func select*(hTest: HTest; cssQuery: string;
             options: set[QueryOption] = DefaultQueryOptions): HTest =
+  ## Selects the first matching element given `cssQuery` or `nil` if can't find anything
   hTest.html = hTest.html.querySelector(cssQuery, options)
   result = hTest
 
 func selectNth*(hTest: HTest; cssQuery: string; n: int;
                options: set[QueryOption] = DefaultQueryOptions): HTest =
+  ## Selects the `n` element found by the given `cssQuery`
   hTest.html = hTest.html.querySelectorAll(cssQuery, options)[n]
   result = hTest
 
 func `not`*(hTest: HTest): HTest =
+  ## Negates the following assert statement
   hTest.useNot = true
   result = hTest
 
 template toContainElement*(hTest: HTest; cssQuery: string) =
+  ## Checks if there is a element described by `cssQuery`
   check hTest.html.querySelector(cssQuery) != nil xor hTest.useNot
 
 template toContainElementsGreaterThan*(hTest: HTest; cssQuery: string; amount: int) =
+  ## Checks if there are more than n `amount` of elements described by `cssQuery`
   check hTest.html.querySelectorAll(cssQuery).len > amount xor hTest.useNot
 
 template toContainElementsGreaterThanOrEqual*(hTest: HTest; cssQuery: string; amount: int) =
+  ## Checks if there are n `amount` or more elements described by `cssQuery`
   check hTest.html.querySelectorAll(cssQuery).len >= amount xor hTest.useNot
 
 template toContainElementsLessThan*(hTest: HTest; cssQuery: string; amount: int) =
+  ## Checks if there are less than n `amount` of elements described by `cssQuery`
   check hTest.html.querySelectorAll(cssQuery).len < amount xor hTest.useNot
 
 template toContainElementsLessThanOrEqual*(hTest: HTest; cssQuery: string; amount: int) =
+  ## Checks if there are n `amount` or less of elements described by `cssQuery`
   check hTest.html.querySelectorAll(cssQuery).len <= amount xor hTest.useNot
 
 template toContainNthElements*(hTest: HTest; cssQuery: string; amount: int) =
+  ## Checks if there are n `amount` of elements described by `cssQuery`
   check hTest.html.querySelectorAll(cssQuery).len == amount xor hTest.useNot
 
 template toHaveAttribute*(hTest: HTest; attribute: string) =
+  ## Checks if the element have the given `attribute`
   check hTest.html.attr(attribute) != "" xor hTest.useNot
 
 template toHaveNthAttributes*(hTest: HTest; amount: int) =
+  ## Checks if the element have n `amount` of attribute`
   check hTest.html.attrsLen == amount xor hTest.useNot
 
 template innerTextToBe*(hTest: HTest; text: string) =
+  ## Checks for the element inner text is equal to `text`
   check hTest.html.innerText == text xor hTest.useNot
 
 template toContainInnerText*(hTest: HTest; text: string) =
+  ## Checks if the inner text of the element contains `text`
   check hTest.html.innerText.contains(text) xor hTest.useNot
 
 template tagToBe*(hTest: HTest; tag: string) =
+  ## Checks for the element tag is equal to `tag`
   check hTest.html.rawTag == tag xor hTest.useNot
